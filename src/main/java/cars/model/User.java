8b66f5e1115ea09e1554f02101bf9dc2539723cd
kemochka <kemaevak@yandex.ru>
2024-07-31 13:40:51 +0300

    2. HQL [#6874]

---
+++ src/main/java/cars/model/User.java
@@ -0,0 +1,16 @@
+package cars.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "auto_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String login;
+    private String password;
+}
