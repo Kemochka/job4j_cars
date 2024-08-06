fcd50fc0c756207660582094a2005b0e1f591ae7
kemochka <kemaevak@yandex.ru>
2024-08-06 17:36:58 +0300

    2. ManyToMany [#331986]

--- src/main/java/cars/model/Post.java
+++ src/main/java/cars/model/Post.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "auto_post")
@@ -18,4 +20,16 @@ public class Post {
     @ManyToOne
     @JoinColumn(name = "auto_user_id")
     private User user;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "post_id")
+    private List<PriceHistory> priceHistory = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "participate",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "post_id")}
+    )
+    private List<User> participates = new ArrayList<>();
 }
