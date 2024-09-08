8274808130b8d6fcd5ca93b90df14f8f24c7974d
kemochka <kemaevak@yandex.ru>
2024-09-08 19:01:49 +0500

    add index

--- src/main/java/cars/controller/IndexController.java
+++ src/main/java/cars/controller/IndexController.java
@@ -2,13 +2,14 @@ package cars.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
 public class IndexController {
-    @GetMapping("index")
-    public String getIndex() {
+    @GetMapping({"/", "/index"})
+    public String getIndex(Model model) {
         return "index";
     }
 }
